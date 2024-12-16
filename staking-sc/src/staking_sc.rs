0ae0bd17ac12ac6e923def34fa49dafb65ad4ea4
VersaliXis <versalix@protonmail.com>
2024-12-16 11:14:22 +0100

    change memory struct for staking position

--- staking-sc/src/staking_sc.rs
+++ staking-sc/src/staking_sc.rs
@@ -4,10 +4,13 @@ multiversx_sc::derive_imports!();
 
 #[type_abi]
 #[derive(TopEncode, TopDecode, PartialEq, Debug, NestedEncode, NestedDecode)]
-pub struct StakeDataObj<M: ManagedTypeApi> {
-	pub token: TokenIdentifier<M>,
-	pub staked_amount: BigUint<M>
+pub struct StakingPositionObj<M: ManagedTypeApi> {
+	pub staked_amount: BigUint<M>,
+    pub stake_block: u64,
 }
+
+pub const BLOCKS_IN_YEAR: u64 = 60 * 60 * 24 * 365 / 6;
+pub const MAX_PERCENTAGE: u64 = 10_000;
 
 #[multiversx_sc::contract]
 pub trait TokenIssuerSc:
@@ -21,9 +24,10 @@ pub trait TokenIssuerSc:
 
     ///////// Storage ///////// 
     /// Stores a set of (token_id, staked_amount) associated to an address
-    #[storage_mapper("issuedTokens")]
+    #[view(getStakingPosition)]
+    #[storage_mapper("stakingPosition")]
     // Stores each issued token associeted with the address of the endpoint issueTokenSnow caller
-    fn staking_data(&self, address: ManagedAddress) -> SetMapper<StakeDataObj<Self::Api>>;
+    fn staking_position(&self, address: ManagedAddress, token_id: TokenIdentifier) -> SingleValueMapper<StakingPositionObj<Self::Api>>;
 
     ///////// Endpoints ///////// 
     /// Allows a user to stake any amount of WINTER-xx token
@@ -34,17 +38,15 @@ pub trait TokenIssuerSc:
         //raises error if multiple esdt payments
         let payment = self.call_value().single_esdt();
         let token_id = payment.token_identifier;
-        let amount = payment.amount;
         let caller = self.blockchain().get_caller();
-
-        for staked_token in self.staking_data(caller.clone()).iter() {
-            let s_token_id = staked_token.token;
-            if s_token_id == token_id {
-                panic!("You have already staked that token, you must unstake before.");
-            }
-        }
-        let staked_data = StakeDataObj{token: token_id, staked_amount: amount};
-        self.staking_data(caller).insert(staked_data);
+        let staking_pos = self.staking_position(caller.clone(), token_id.clone());
+        
+        require!(staking_pos.is_empty(), "You have already staked that token, you must unstake before.");
+        let current_block = self.blockchain().get_block_nonce();
+        let amount = payment.amount;
+        
+        let new_staking_pos = StakingPositionObj{stake_block: current_block, staked_amount: amount};
+        staking_pos.set(new_staking_pos);
     }
 
 }
